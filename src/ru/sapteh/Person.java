6e344a27c7dd22c4e3e68ce5203b31725d17e3ea
smalysheff <36011581+smalysheff@users.noreply.github.com>
2020-09-29 13:09:19 +0300

    Update Person.java
    Adding constructor Person

--- src/ru/sapteh/Person.java
+++ src/ru/sapteh/Person.java
@@ -3,4 +3,9 @@ package ru.sapteh;
 public class Person {
     private String name;
     private int age;
+    
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 }
